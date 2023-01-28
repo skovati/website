d8709fd71663f51fdf1376be089c8b9ed1fdf864
skovati <skovati@protonmail.com>
2023-01-27 20:20:05 -0600

    inline hue.js

--- static/hue.js
+++
@@ -1,9 +0,0 @@
-const root = document.querySelector(":root");
-
-let hue = sessionStorage.getItem("hue");
-if (!hue) {
-    hue = Math.floor(Math.random() * 360);
-    sessionStorage.setItem("hue", hue)
-}
-
-root.style.setProperty("--hue", (parseInt(hue) + 1) % 360);
